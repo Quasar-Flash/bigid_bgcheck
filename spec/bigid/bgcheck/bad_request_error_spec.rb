a47b3d932738390821ed519863dcddced5f0414c
Danilo Carolino <danilogcarolino@gmail.com>
2021-06-15 12:08:29 -0300

    feat: add missing spec files

---
+++ spec/bigid/bgcheck/bad_request_error_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Bigid::Bgcheck::BadRequestError do
+  include_examples "error"
+end
---
+++ spec/bigid/bgcheck/no_info_error_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Bigid::Bgcheck::NoInfoError do
+  include_examples "http_error"
+end
